7363f525640c330286c458443a600598472f8626
Christopher Johnson <cjohn@ceh.ac.uk>
2014-09-30 11:03:56 +0100

    Customising the vocabs.nerc.ac.uk.ttl to use lda-assets Fixing provision script to start up cleanly git ignoring the vagrant box

--- shell/provision.sh
+++ shell/provision.sh
@@ -2,13 +2,13 @@
 # and link vagrant files into place
 
 apt-get update
-apt-get install tomcat7
+apt-get install -y tomcat7
 rm -rf /var/lib/tomcat7/webapps/ROOT
 
 # Link Elda Config into place
 mkdir -p /etc/elda/conf.d
-ln -s /etc/elda/conf.d/ROOT /vagrant/elda-config
-ln -s /var/lib/tomcat7/webapps/lda-assets /vagrant/lda-assets
+ln -s /vagrant/elda-config /etc/elda/conf.d/ROOT
+ln -s /vagrant/lda-assets /var/lib/tomcat7/webapps/lda-assets
 
-wget http://repository.epimorphics.com/com/epimorphics/lda/elda-common/1.2.35/elda-common-1.2.35.war -O /var/lib/tomcat7/webapps/ROOT.war
+wget http://repository.epimorphics.com/com/epimorphics/lda/elda-common/1.2.35/elda-common-1.2.35.war -q -O /var/lib/tomcat7/webapps/ROOT.war
 service tomcat7 restart
